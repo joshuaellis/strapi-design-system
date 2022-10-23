78f56186b31c5fab346ffb9bc97f0ace568de76d
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-10-23 19:54:25 +0100

    feat: add vite setup

--- packages/strapi-design-system/vite.config.js
+++ packages/strapi-design-system/vite.config.js
@@ -1,38 +1,34 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import glob from 'tiny-glob';
 
-export default defineConfig({
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, './src/index.js'),
-      name: 'StrapiDesignSystem',
-      // the proper extensions will be added
-      fileName: 'strapi-design-system',
-      formats: ['cjs', 'es'],
+export default glob('./src/**/!(*.spec|*.e2e).{js,svg}').then(async (paths) => {
+  return defineConfig({
+    esbuild: {
+      loader: 'jsx',
+      include: /src\/.*\.jsx?$/,
+      exclude: [],
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['react', 'react-dom', 'react-router-dom', 'styled-components', '@strapi/icons'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-          ['react-dom']: 'ReactDOM',
-          ['react-router-dom']: 'ReactRouterDOM',
-          'styled-components': 'styled',
-          '@strapi/icons': 'StrapiIcons',
+    build: {
+      target: 'es2020',
+      lib: {
+        formats: ['cjs', 'es'],
+        fileName: (format) => {
+          return `[name].${format === 'es' ? 'js' : format}`;
+        },
+      },
+      rollupOptions: {
+        input: [resolve(__dirname, './src/index.js'), ...paths.map((path) => `./${path}`)],
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: (id) => !id.startsWith('.') && !id.startsWith('/'),
+        output: {
+          dir: 'dist',
+          preserveModules: true,
         },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  });
 });
--- packages/strapi-icons/vite.config.js
+++ packages/strapi-icons/vite.config.js
@@ -10,23 +10,19 @@ export default defineConfig({
   },
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, './src/index.js'),
-      name: 'StrapiIcons',
-      // the proper extensions will be added
-      fileName: 'strapi-icons',
       formats: ['cjs', 'es'],
+      fileName: (format) => {
+        return `[name].${format === 'es' ? 'js' : format}`;
+      },
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react'],
+      external: (id) => !id.startsWith('.') && !id.startsWith('/'),
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-        },
+        dir: 'dist',
+        preserveModules: true,
       },
     },
   },
